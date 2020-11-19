8a45ca2204247e72a23ec759ca7b185303ebc2dc
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-11-19 15:45:18 +0100

    loops to delete duplicates in list

--- DOM.js
+++ DOM.js
@@ -2,6 +2,8 @@ const ul = document.querySelector("ul");
 
 let children = ul.childNodes;
 
+let i = 0;
+
 for (let child of children) {
     if (child.textContent == "Fast and Furious") {
         const firstChild = ul.firstElementChild;
@@ -9,5 +11,20 @@ for (let child of children) {
         child.className = "important";
     }
 
-    child.addEventListener("click", function(){alert(child.textContent);})
+    child.addEventListener("click", function () {
+
+        if (child.textContent == "Fast and Furious") {
+            alert("The most important franchise ever, the story of DOM(inic) Toretto's family. It's not about car, it's about family.")
+        } else {
+            alert(child.textContent);
+        };
+    })
+
+    for (j = i + 1; j < children.length; j++) {
+        if (child.isEqualNode(children[j])) {
+            ul.removeChild(children[j]);
+        }
+    }
+
+    i++;
 }
