7e65a4465d65fb0fc512d443011a6e490db0e83d
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-11-30 09:40:11 +0100

    typo correctio

--- 4.regex/RegEx1.js
+++ 4.regex/RegEx1.js
@@ -1,4 +1,4 @@
-// Write a regex to replace all occurences of Emily with Kelian.
+// Write a regex to replace all occurrences of Emily with Kelian.
 
 const text = `Emily is probably the best coach I've ever had. I'm an absolute fan of Emily's exercices on regular expressions. 
 I wouldn't be as fluent in JavaScript if I had not crossed Emily's path`
