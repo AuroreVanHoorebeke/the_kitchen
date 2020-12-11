10ddf6d7bffd615ea017d13d94538b881c977e9c
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-12-11 14:39:15 +0100

    ex1 done

--- 2.timeouts_and_interval/ex1/ex1.js
+++ 2.timeouts_and_interval/ex1/ex1.js
@@ -2,3 +2,34 @@
 AIM: Write a function that mimics the behaviour of a typewriter.
 Using setInterval display the word prout one character at a time (one letter per second). Once the word is written on the screen clear the interval.
 */
+
+const body = document.querySelector("body");
+
+const paragraph = document.createElement("p");
+body.appendChild(paragraph);
+
+
+let interval;
+const word = "prout";
+
+let wordArr = word.split("");
+console.log(wordArr);
+let i = 0;
+
+interval = setInterval(displayLetter, 1000);
+
+displayLetter()
+function displayLetter(){
+    console.log("in function");
+    let newWord = wordArr[i];
+    i++;
+
+    if(wordArr[i] == undefined){
+        paragraph.textContent += newWord;
+        clearInterval(interval);
+        console.log("cleared");
+    } else {
+        console.log(newWord);
+        paragraph.textContent += newWord;
+    }
+}
