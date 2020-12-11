2964d39eb5de2251221616403f71ad37fbdb639b
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-12-11 11:21:26 +0100

    first commit

---
+++ 2.timeouts_and_interval/ex1/ex1.js
@@ -0,0 +1,4 @@
+/*
+AIM: Write a function that mimics the behaviour of a typewriter.
+Using setInterval display the word prout one character at a time (one letter per second). Once the word is written on the screen clear the interval.
+*/
