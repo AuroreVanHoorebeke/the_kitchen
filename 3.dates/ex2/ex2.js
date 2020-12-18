4ba3c882aa1c0ea3bc67052272d8f0ba0e6d44f3
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-12-18 16:25:26 +0100

    fixed days problem

--- 3.dates/ex2/ex2.js
+++ 3.dates/ex2/ex2.js
@@ -5,9 +5,13 @@ Write a function to find how many days have passed since any point in time (afte
 const sinceBirth = document.querySelector(".sinceBirth");
 const sinceUndefined = document.querySelector(".sinceUndefined");
 
+let now = new Date();
+let nowDate = now/(1000*60*60*24)
+
+console.log(now);
 function getDaysSinceBirth(){
     let birthDate =  new Date('1998-01-13')
-    const sinceBirthTime = birthDate.getTime()/(1000*60*60*24);
+    const sinceBirthTime = Math.floor(nowDate - (birthDate.getTime()/(1000*60*60*24)));
 
     sinceBirth.textContent = `${sinceBirthTime} days have passed since my birth.`
 }
@@ -18,14 +22,16 @@ function getDaysSinceUndefined(){
     // let dayUser = question.readlineSync("Enter the day (e.g. 24: ");
     // let monthUser = question.readlineSync("Enter the month (e.g. 12): ");
     // let yearUser = question.readlineSync("Enter the year (e.g. 2004): ");
+
+    //Web version
     alert("We are going to calculate how many days have passed since a date of your choice in the past.");
-    let dayUser = prompt("Enter the day (e.g. 24: ");
-    let monthUser = prompt("Enter the month (e.g. 12): ");
-    let yearUser = prompt("Enter the year (e.g. 2004): ");
+    let dayUser = Number(prompt("Enter the day (e.g. 24: "));
+    let monthUser = Number(prompt("Enter the month (e.g. 12): "));
+    let yearUser = Number(prompt("Enter the year (e.g. 2004): "));
 
     
     let undefinedDate =  new Date(`${yearUser}-${monthUser}-${dayUser}`);
-    const sinceUndefinedTime = undefinedDate.getTime()/(1000*60*60*24);
+    const sinceUndefinedTime = Math.floor(nowDate - (undefinedDate.getTime()/(1000*60*60*24)));
 
     sinceUndefined.textContent = `${sinceUndefinedTime} days have passed since your date.`
 }
